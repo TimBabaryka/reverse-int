55a0af07ad08d5368e1b87829eafaf2928a0849b
Tim Babaryka <timasky95@gmail.com>
2021-08-21 20:37:13 +0300

    feat:js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
+
+    if (n < 0) {
+      let string1= n * -1;
+      let string2 = string1.toString().split('').reverse().join('');
+      return string2;
+
+      } else {
   
-}
+            let string = n.toString().split('').reverse().join('');
+            return string;
+            
+      }
+    }
